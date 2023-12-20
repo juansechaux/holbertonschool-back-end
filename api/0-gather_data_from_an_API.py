1d8bd302c2aaa3a59c68c0e6e9f18e7f8176f52d
juansechaux <juansechaux@hotmail.com>
2023-12-20 16:45:24 -0500

    Task 0, Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress, create a function, update documentation, refactory python code style

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -32,14 +32,20 @@ def display_info():
         num_of_task = 0
         num_of_complete_task = 0
         for dict in todos_data:
-            if dict.get("userId") == int(employee_id) and dict.get("completed"):
+            if dict.get("userId") == int(employee_id)\
+               and dict.get("completed"):
                 list_complete_task.append(dict.get("title"))
                 num_of_complete_task += 1
             if dict.get("userId") == int(employee_id):
                 num_of_task += 1
-    print(f"Employee {user_name} is done with tasks({num_of_complete_task}/{num_of_task}):")
+    # print(f"Employee {user_name} is done
+    # with tasks({num_of_complete_task}/{num_of_task}):")
+    all_text = f"Employee {user_name} is done with tasks"
+    print(f"{all_text}({num_of_complete_task}/{num_of_task}):")
     for task in list_complete_task:
         print(f"\t {task}")
 
+
 if __name__ == "__main__":
+
     display_info()

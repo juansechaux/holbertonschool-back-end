706e994c353f41348c92df22a9695891578e7c52
juansechaux <juansechaux@hotmail.com>
2023-12-20 16:18:44 -0500

    Task 0, Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''
+"""
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her todo list progress.
-'''
+"""
 import requests
 from sys import argv
 

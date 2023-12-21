603afa7bf38cac454b5fead79bdaa4ba61989e6f
juansechaux <juansechaux@hotmail.com>
2023-12-20 19:51:00 -0500

    Task 1, Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,9 +3,9 @@
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her todo list progress.
 """
+import csv
 import requests
 from sys import argv
-import csv
 
 
 def display_info():
